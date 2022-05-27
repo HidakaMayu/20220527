2f920a18e7ce63d933db660c3ed2f746a8b07853
RAK_kanato <tennbu1@gmail.com>
2022-05-27 17:41:59 +0900

    落下と素早さのプログラムが終わった

--- Assets/Ohno/Item/ArrowsPrefab.cs
+++ Assets/Ohno/Item/ArrowsPrefab.cs
@@ -8,6 +8,10 @@ public enum PhaseType
     Phase2,
     Phase3,
     Phase4,
+    Phase5,
+    Phase6,
+    Phase7,
+    Phase8,
 }
 
 public class ArrowsPrefab : MonoBehaviour
@@ -15,10 +19,20 @@ public class ArrowsPrefab : MonoBehaviour
     public GameObject ArrowPrefab;
 
     float time = 0;
-    float longtime = 0;
-    float overTime = 1;
+    //float longtime = 0;
+    float overTime = 3;
     public float height;
     //public float time = 0f;
+
+    private void OnEnable()
+    {
+        GameManager.OnChangeGenerateTime += ChangeGenerateTime;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnChangeGenerateTime -= ChangeGenerateTime;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +51,7 @@ public class ArrowsPrefab : MonoBehaviour
         //}
 
         time += Time.deltaTime;
-        longtime += time;
+        //longtime += time;
         if(time >= overTime)
         {
             float x = Random.Range(-8.0f, 8.0f);
@@ -46,46 +60,47 @@ public class ArrowsPrefab : MonoBehaviour
             time = 0;
         }
 
-        if(longtime >= 50)
-        {
-            if (overTime >= -500)
-            {
+        //if(longtime >= 50)
+        //{
+        //    if (overTime >= -500)
+        //    {
 
-                //if (overTime <= 0.5)
-                //{
-                    overTime -= 0.0001f;
-                //}
-                //if (overTime <= 0.4)
-                //{
-                //    overTime -= 0.00001f;
-                //}
-                //if (overTime <= 0.3)
-                //{
-                //    overTime -= 0.000001f;
-                //}
-                //if (overTime <= 0.2)
-                //{
-                //    overTime -= 0.000001f;
-                //}
-                //if (overTime <= 0.1)
-                //{
-                //    overTime -= 0.0000001f;
-                //}
-                Debug.Log(overTime);
-            }
+        //        //if (overTime <= 0.5)
+        //        //{
+        //            overTime -= 0.0001f;
+        //        //}
+        //        //if (overTime <= 0.4)
+        //        //{
+        //        //    overTime -= 0.00001f;
+        //        //}
+        //        //if (overTime <= 0.3)
+        //        //{
+        //        //    overTime -= 0.000001f;
+        //        //}
+        //        //if (overTime <= 0.2)
+        //        //{
+        //        //    overTime -= 0.000001f;
+        //        //}
+        //        //if (overTime <= 0.1)
+        //        //{
+        //        //    overTime -= 0.0000001f;
+        //        //}
+        //        Debug.Log(overTime);
+        //    }
 
-            
-        }
 
+        //}
+        Debug.Log(overTime);
     }
 
-    void reset()
-    {
-        overTime = 1;
-    }
+    //void reset()
+    //{
+    //    overTime = 1;
+    //}
 
     void ChangeGenerateTime(PhaseType type)
     {
+        //Debug.Log($"現在のフェーズ{type}");
         switch (type)
         {
             case PhaseType.Phase1:
@@ -98,7 +113,19 @@ public class ArrowsPrefab : MonoBehaviour
                 overTime = 1;
                 break;
             case PhaseType.Phase4:
-                overTime = 0;
+                overTime = 0.5f;
+                break;
+            case PhaseType.Phase5:
+                overTime = 0.3f;
+                break;
+            case PhaseType.Phase6:
+                overTime = 0.15f;
+                break;
+            case PhaseType.Phase7:
+                overTime = 0.05f;
+                break;
+            case PhaseType.Phase8:
+                overTime = 0.001f;
                 break;
             default:
                 break;
