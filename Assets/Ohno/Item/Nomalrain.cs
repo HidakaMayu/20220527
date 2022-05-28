111d01cbe63b6ce3750a8b31142ff1c7deb79507
RAK_kanato <tennbu1@gmail.com>
2022-05-28 13:41:34 +0900

    とりあえず

---
+++ Assets/Ohno/Item/Nomalrain.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nomalrain : MonoBehaviour
+{
+    [SerializeField] GameObject _enemy;
+    PlayerController playerController;
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            playerController = GetComponent<PlayerController>();
+            playerController.Damage(1);
+            Destroy(this.gameObject);
+        }
+        else if(other.gameObject.tag == "Floor")
+        {
+            Debug.Log($"‚ ‚½‚Á‚½");
+
+            Vector3 position = new Vector3(transform.position.x, transform.position.y, 0);
+            Instantiate(_enemy, position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
